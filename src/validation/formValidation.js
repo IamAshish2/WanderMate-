96a55cf0de34ea9be9a4b4dde568e854cb720694
Ashish karki <134418831+IamAshish2@users.noreply.github.com>
2024-08-16 10:43:24 +0545

    typo fixed for signin

--- src/validation/formValidation.js
+++ src/validation/formValidation.js
@@ -5,11 +5,15 @@ export const signUpSchema = yup.object().shape({
     email:yup.string().email("Enter a valid email").required("Email cannot be empty"),
     password:yup.string().min(8,"password should be at least 8 characters long").max(50,"Password should not be longer than 50 characters"),
     confirmPassword:yup.string().oneOf([yup.ref("password"),null],"Passwords do not match."),
+    role:yup.string().required("the role is required"),
     IAgree:yup.bool().oneOf([true],"You must agree to all Terms and Conditions")
 });
 
 
 export const signInSchema = yup.object().shape({
-    username:yup.string().required("Username cannot be empty"),
-    passowrd:yup.string().min(8,'passwords should be at least 8 characters long.').max(50,"Password should not be longer than 50 characters").required(),
+    username: yup.string().required("Username cannot be empty"),
+    password: yup.string()
+        .min(8, 'Passwords should be at least 8 characters long.')
+        .max(50, 'Password should not be longer than 50 characters')
+        .required('Password is required'),
 });
