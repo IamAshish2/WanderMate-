b136a0ea4adcf430787ff07d3d404d67f0608503
Ashish karki <134418831+IamAshish2@users.noreply.github.com>
2024-08-16 22:00:01 +0545

    set the url to api's endpoint and put check for authorization for users

--- src/API/index.js
+++ src/API/index.js
@@ -6,9 +6,16 @@ const jsonUrl = "http://localhost:5156"
 
 export const getHotels = async() =>  {
     try{
-        const response = await axios.get(`${jsonUrl}/api/Hotel`)
+        const token = localStorage.getItem("token");
+        let response;
+        if (token) {
+          response = await axios.get(`${jsonUrl}/api/Hotel`, {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          });
+        }
         const data = await response.data
-        // console.log(data);
         return data
     }catch(err){
         console.log(err)
@@ -18,8 +25,18 @@ export const getHotels = async() =>  {
 
 export const getHotel = async(id) => {
     try{
-        const response = await axios.get(`${jsonUrl}/hotels/${id}`)
+        const token = localStorage.getItem("token");
+        let response;
+        if (token) {
+          response = await axios.get(`${jsonUrl}/api/Hotel/${id}`, {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          });
+        }
+        // const response = await axios.get(`${jsonUrl}/hotels/${id}`)
         const data = await response.data
+        console.log(data);
         return data
     }catch(err){
         console.log(err)
@@ -28,8 +45,18 @@ export const getHotel = async(id) => {
 
 export const getTravelPackages = async() => {
     try{
-        const response = await axios.get(`${jsonUrl}/api/TravelPackages`)
+        // const response = await axios.get(`${jsonUrl}/api/TravelPackages`)
+        const token = localStorage.getItem("token");
+        let response;
+        if (token) {
+          response = await axios.get(`${jsonUrl}/api/TravelPackages`, {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          });
+        }
         const data =await response.data
+
         return data
     }catch(err){
         console.log(err)
@@ -39,7 +66,16 @@ export const getTravelPackages = async() => {
 //http://localhost:5156/api/TravelPackages
 export const getTravelPackage = async(id) => {
     try{
-        const response = await axios.get(`${jsonUrl}/api/TravelPackages/${id}`)
+        // const response = await axios.get(`${jsonUrl}/api/TravelPackages/${id}`)
+        const token = localStorage.getItem("token");
+        let response;
+        if (token) {
+          response = await axios.get(`${jsonUrl}/api/TravelPackages/${id}`, {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          });
+        }
         const data =await response.data
         return data
     }catch(err){
@@ -49,7 +85,16 @@ export const getTravelPackage = async(id) => {
 
 export const getDestination = async(id) => {
     try{
-    const response = await axios.get(`${jsonUrl}/topDestinations/${id}`)
+        const token = localStorage.getItem("token");
+        let response;
+        if (token) {
+          response = await axios.get(`${jsonUrl}//topDestinations/${id}`, {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          });
+        }
+    // const response = await axios.get(`${jsonUrl}`)
     const data = await response.data
     return data
     }catch(err){
@@ -59,7 +104,15 @@ export const getDestination = async(id) => {
 
 export const getTopDestinations = async() => {
     try{
-    const response = await axios.get(`${jsonUrl}/topDestinations`)
+        const token = localStorage.getItem("token");
+        let response;
+        if (token) {
+          response = await axios.get(`${jsonUrl}/api/topDestinations`, {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          });
+        }
     const data = await response.data
     return data
     }catch(err){
@@ -69,7 +122,16 @@ export const getTopDestinations = async() => {
 
 export const getThingsToDo = async() => {
     try{
-    const response = await axios.get(`${jsonUrl}/thingsToDo`)
+        const token = localStorage.getItem("token");
+        let response;
+        if (token) {
+          response = await axios.get(`${jsonUrl}/api/thingsToDo`, {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          });
+        }
+    // const response = await axios.get(`${jsonUrl}/thingsToDo`)
     const data =await response.data
     return data
     }catch(err){
@@ -99,7 +161,7 @@ export const getReview = async(hotelId) => {
 
 export const getUsers = async() => {
     try{
-    const response = await axios.get(`${jsonUrl}/users`)
+    const response = await axios.get(`${jsonUrl}/api/User`)
     const data = await response.data
     return data
     }catch(err){
@@ -107,12 +169,15 @@ export const getUsers = async() => {
     }
 }
 
-export const getUser = async(userId) => {
-    try{
-    const response = await axios.get(`${jsonUrl}/users/${userId}`)
-    const data = await response.data
-    return data
-    }catch(err){
-        console.log(err)
+export const getUser = async (id) => {
+    try {
+        console.log(id);
+        const response = await axios.get(`${jsonUrl}/api/User/${id}`);
+        console.log(response);
+        return response.data;
+    } catch (err) {
+        console.error('Error fetching user:', err.response ? err.response.data : err.message);
+        throw err; // Optionally rethrow the error if you need to handle it further up
     }
 }
+
